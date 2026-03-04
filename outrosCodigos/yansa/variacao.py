5682a97215a3940872405390de63de7bbe84de79
Renan Aryel <renanaryel@gmail.com>
2026-03-04 00:31:07 -0300

    codigo de variacao com recurssao

--- outrosCodigos/yansa/variacao.py
+++ outrosCodigos/yansa/variacao.py
@@ -11,31 +11,53 @@ with open('fm0229.csv', mode='r') as file:
 # print(len(campo))
 
 variacao = []
+tempo = []
 p = 0
 pi = 0
-k = len(campo)
+eAtual = 0
+eAntes = 0
 
-def calcVariacao(k):
-    global variacao, p, pi, campo
+def calcVariacao(k, m = 0):
+    global variacao, p, pi, campo, tempo, eAtual, eAntes
 
-    for i in range(1, k+1, 6):
-        # if i >= k+1:
-        #     return variacao, p, pi
+    if m >= k:
+        return variacao, p, pi, eAtual, eAntes
+
+    for i in range(m, k):
         if i == 0:
             variacao.append(0)
-        else:
-            var = campo[i] - campo[i-1]
-            variacao.append(var)
-            if var >= 50 and var < 100:
-                p += 1
-            if var >= 100:
-                pi += 1
-        # calcVariacao(k)
-    return variacao, p, pi
+            tempo.append(0)
+        else:        
+            eAtual = campo[i]
+            eAntes = campo[i-1]
 
-calcVariacao(k) 
+        var = eAtual - eAntes
+        variacao.append(var)
+        tempo.append(i)
+        m += 1
 
-x = list(range(len(variacao)))
+        if var >= 50 and var < 100:
+            p += 1
+            # m += 6
+
+        if var >= 100:
+            pi += 1
+            # m += 6
+
+    return calcVariacao(k, m)
+
+k = len(campo)
+calcVariacao(k)
+# print(campo[k])
+# print("O valor da posição 692 é: ", variacao[159])
+# print("Campo anterior: ", eAntes)
+# print("Campo atual: ", eAtual)
+
+# print("campo real anterior: ", campo[157])
+# print("campo real atual: ", campo[158])
+
+
+# x = list(range(len(variacao)))
 print("Quantidade de pulsos: ", p)
 print("Quantidade de pulsos intensos: ", pi)
 
@@ -49,12 +71,15 @@ plt.figure(figsize=(8,5))
 # plt.ion()
 
 # plt.title('variações de campo e pulsos', fontsize=16, fontweight='bold', fontstyle='italic', fontfamily='monospace')
-plt.title('variações de campo e pulsos', fontsize=16, fontweight='bold', fontfamily='monospace')
+plt.title('Variações de campo e pulsos - Python', fontsize=16, fontweight='bold', fontfamily='monospace')
 plt.xlabel('tempo', fontsize=10, fontfamily='monospace')
 plt.ylabel('V/m', fontsize=10, fontfamily='monospace')
-plt.tight_layout()
-plt.scatter(x, variacao, label='pulsos')
+# plt.tight_layout()
+
+legenda = "Pulsos: " + str(p) + "\n PI: " + str(pi)
+
 # plt.scatter(x, variacao, label="pulsos")
+plt.scatter(tempo, variacao, label=legenda)
 plt.legend(fontsize=12, frameon=True, framealpha=0.7 , facecolor='white')
 plt.show()
 
